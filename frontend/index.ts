136f5692659da1890ae976a5d96dd3fb1c500121
Malformed <enderman08@gmail.com>
2026-02-13 22:17:10 +0300

    misc

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!")
+import { createOpenAICompatible } from '@ai-sdk/openai-compatible'
+import 'dotenv/config'
+
+const provider = createOpenAICompatible({
+  name: 'local',
+  baseURL: process.env.BASE_URL!,
+  supportsStructuredOutputs: true,
+  transformRequestBody: undefined, // TODO
+})
